796784efd2ed595f12c0c1a45e6e9054455fe67d
SeanAbbott1 <sabbott2012@gmail.com>
2016-02-20 23:20:47 -0500

    Sean Abbott Week 7 Submit 1

--- q2.py
+++ q2.py
@@ -20,13 +20,21 @@ def playGame():
             print('Computer lost!')
             break
         
-
 def askUserChoice():
     # TODO: write code in this functiont that:
     # 1. Asks the user to enter their input (between 1 and 4)
     # 2. Checks that the user's input is valid. If it's not valid (if it's not between 1 and 4), then ask the user to re-enter their input.
     # 3. Once the user enters a valid input, return that input as an integer.
 
+    myNumbers = [1,2,3,4]
+    while True:
+        choice = int(input('Pick a number in range 1-4? '))
+        # Check if input is in range
+        if choice in myNumbers:
+            return choice
+            break
+        else:
+            print ('Out of range. Re-enter number again! ')
 
 def subtractSticks( number ):
     global sticks 
@@ -35,6 +43,13 @@ def subtractSticks( number ):
     # 1. subtracts the parameter `number` from the global variable `sticks`
     # 2. checks if the number subtracted resulted in the last stick, if so, return True
     # 3. if there are still sticks left, return False
-    
+
+    sticksTaken=sticks-number
+    if sticksTaken==1:
+        return True
+    elif sticksTaken >=5 or sticksTaken <=0:
+        return False
+
 def determineComputerChoice():
     # TODO: write code inside this function that returns an integer between 1 and 4, random chosen by the computer
+    return random.randint(1,4)
